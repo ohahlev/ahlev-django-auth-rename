a9946bb35f7be19cb800c7f3ccb889f57bde9ca4
ahlev <ohahlev@gmail.com>
2020-01-21 00:32:11 +0700

    remove style from version label

--- auth_rename/apps.py
+++ auth_rename/apps.py
@@ -3,4 +3,4 @@ from django.utils.html import format_html
 from . import __version__ as VERSION
 
 class AuthRenameConfig(AuthConfig):
-    verbose_name = format_html("User Management <span class='version'>{}</span>", VERSION)
+    verbose_name = format_html("User Management {}", VERSION)
